ffaaeb53a4f473a8862e56998c40cd146f7be6d2
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-05-02 20:57:17 +0700

    Step 4.3: Attribute Routing with Http[Verb] Attributes

--- server/Controllers/PostController.cs
+++ server/Controllers/PostController.cs
@@ -7,9 +7,23 @@ namespace server.Controllers
     {
         // GET: /api/post
         [HttpGet]
-        public ActionResult<string> Index()
+        public ActionResult<string> List()
         {
-            return "Index";
+            return "Get A List";
+        }
+
+        // GET: /api/post/7
+        [HttpGet("{id:int}")]
+        public ActionResult<string> Item(int id)
+        {
+            return "Get An Item";
+        }
+
+        // POST: /api/post
+        [HttpPost]
+        public ActionResult<string> AddPost()
+        {
+            return "Add a new post";
         }
     }
 }
