8128b4f48cd4c198b500fbdcda1954bf45f590ae
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2019-05-04 01:22:02 +0700

    Step 8.3: Create a simple controller using Realm

--- server/Controllers/BaseController.cs
+++ server/Controllers/BaseController.cs
@@ -2,9 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace server.Controllers
 {
-    /// <summary>
-    /// Base Controller
-    /// </summary>
     [Route("api/[controller]")]
     public abstract class BaseController : Controller { }
 }
--- server/Controllers/PostController.cs
+++ server/Controllers/PostController.cs
@@ -1,30 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using Realms;
+using server.Models;
 
 namespace server.Controllers
 {
     /// <summary>
     /// Post Controller
     /// </summary>
+    [SwaggerTag(
+        "Post",
+        Description = "Quản lý hành động của đối tượng bài đăng"
+    )]
     public class PostController : BaseController
     {
+        private Realm realm => Realm.GetInstance();
+        private IQueryable<Post> list => realm.All<Post>();
+        private int nextId => list.Count() == 0 ? 1 : list.AsEnumerable().Max(p => p.Id) + 1;
+
         /// <summary>
-        /// Get A List of Post
+        /// Trả về một danh sách các bài viết
         /// </summary>
         [HttpGet]
-        public ActionResult<string> Index() { return "A list"; }
+        public ActionResult<List<Post>> Index() => list.ToList();
 
         /// <summary>
-        /// Get a Post
+        /// Lấy một bài đăng
         /// </summary>
-        /// <param name="id">Post Id</param>
+        /// <param name="id">Id bài viết</param>
+        /// <response code="200">Tìm thấy</response>
+        /// <response code="404">Không tìm thấy</response>
         [HttpGet("{id:int}")]
-        public ActionResult<string> Item(int id) { return "An item"; }
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        public ActionResult<Post> Item(int id)
+        {
+            var post = realm.Find<Post>(id);
+            if (post == null) return NotFound("Item not found");
+            return post;
+        }
 
         /// <summary>
-        /// Create a new Post
+        /// Đăng một bài viết mới, và trả về bài viết đó
         /// </summary>
-        /// <returns></returns>
+        /// <response code="201">Thành công</response>
         [HttpPost]
-        public ActionResult<string> Post() { return "Post"; }
+        public async Task<ActionResult<Post>> Post()
+        {
+            var post = new Post()
+            {
+                Id = nextId,
+                Content = "Content",
+                Title = " Title",
+                Created = new DateTimeOffset()
+            };
+            await realm.WriteAsync(r => post = r.Add(post));
+            return CreatedAtAction(nameof(Post), post);
+        }
     }
 }
