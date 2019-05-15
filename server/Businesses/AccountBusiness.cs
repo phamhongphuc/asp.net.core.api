91fe7d611913e5cf60a92d199b978bee03235d09
thaotram <sawa.kisei@gmail.com>
2019-05-15 22:25:16 +0700

    Create AccountController

---
+++ server/Businesses/AccountBusiness.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using server.DataAccesses;
+using server.DataTransfers.AccountDataTransfers;
+using server.Middleware.Error;
+using server.Models;
+
+namespace server.Businesses
+{
+    public static class AccountBusiness
+    {
+        public static IQueryable<Account> List => AccountDataAccess.List;
+
+        public static Account Get(int id)
+        {
+            var account = AccountDataAccess.Get(id);
+            if (account == null) throw new Error404NotFound<Account>(id);
+
+            return account;
+        }
+
+        private static void CheckValid(Account account)
+        {
+            // kiểm tra name, pass, email
+            if(account.Name.Length < 3)
+                throw new Error400BadRequest<Account>(
+                    "Tên phải có ít nhất 3 kí tự"
+                );
+        }
+
+        public static async Task<Account> Add(Account account)
+        {
+            var accountInDatabase = AccountDataAccess.GetByEmail(account.Email);
+            if (accountInDatabase != null) throw new Exception("Email " + account.Email + " đã có người sử dụng");
+
+            CheckValid(account);
+            return await AccountDataAccess.Add(account);
+        }
+
+    }
+}
---
+++ server/DataAccesses/AccountDataAccess.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using server.DataAccesses.Base;
+using server.Models;
+using server.Models.Enums;
+
+namespace server.DataAccesses
+{
+    public class AccountDataAccess : ModelHasIdDataAccess<Account>
+    {
+        public static Account GetByEmail(string email)
+        {
+            var x = List.Where(account => account.Email == email);
+            if(x.Count() == 0) return null;
+            return x.First();
+        }
+
+        public static async Task<Account> Add(Account account)
+        {
+            await Database.WriteAsync(realm =>
+            {
+                account.Id = NextId;
+                account.Access = EnumAccess.NormalUser;
+                account.Joined = DateTimeOffset.Now;
+                account = realm.Add(account);
+            });
+            return account;
+        }
+    }
+}
