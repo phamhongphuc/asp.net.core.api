4aaf75546dece43349d3dfe7cef8615bd17fa67e
thaotram <sawa.kisei@gmail.com>
2019-05-17 21:22:22 +0700

    Add CurrentUser

--- server/Controllers/BaseController.cs
+++ server/Controllers/BaseController.cs
@@ -1,7 +1,21 @@
+using System.Linq;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using server.Businesses;
+using server.Models;
 
 namespace server.Controllers
 {
     [Route("api/[controller]")]
-    public abstract class BaseController : Controller { }
+    public class BaseController : Controller
+    {
+        public Account CurrentUser
+        {
+            get
+            {
+                var email = User.Claims.First(i => i.Type == ClaimTypes.Email).Value;
+                return AccountBusiness.GetByEmail(email);
+            }
+        }
+    }
 }
