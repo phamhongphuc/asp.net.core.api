d55956f2d5b2ebac0ce9d9667e6f4db0bdd1da1c
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2019-06-14 20:39:53 +0700

    Config plugins

--- blog.client/index.d.ts
+++ blog.client/index.d.ts
@@ -1,9 +1,18 @@
 import '@nuxtjs/axios';
-import VueRouter from 'vue-router';
 import 'bootstrap-vue';
+import VueRouter from 'vue-router';
+import { $tryCatch } from '~/modules/error-handling';
+import { RootState } from './store';
 
 declare module 'vuex' {
     interface Store<S> {
         $router: VueRouter;
+    }
+}
+
+declare module 'vue/types/vue' {
+    interface Vue {
+        $tryCatch: typeof $tryCatch;
+        $store: Store<RootState>;
     }
 }
